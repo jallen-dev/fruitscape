209d50ce1ac0bd8ef1477ca4a79ac75f4e195514
Justin Allen <jallen@jallen.dev>
2023-12-27 20:20:48 -0500

    Add characters

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,14 +1,18 @@
 import { Sprite, useApp } from "@pixi/react";
+import { ALL_CHARACTERS } from "../characters";
 
 export function Character() {
   const app = useApp();
 
+  const character = ALL_CHARACTERS[Math.floor(Math.random() * ALL_CHARACTERS.length)];
+
   return (
     <Sprite
-      image="https://pixijs.io/pixi-react/img/bunny.png"
+      image={character}
       x={app.screen.width / 2}
       y={app.screen.height / 2}
       anchor={{ x: 0.5, y: 0.5 }}
+      scale={2}
     />
   );
 }
