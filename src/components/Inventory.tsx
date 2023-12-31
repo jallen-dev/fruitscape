7d318aa08d9252e3f7d70d05dd04ebd224548fb4
Justin Allen <jallen@jallen.dev>
2023-12-31 00:40:34 -0500

    Extract FruitQuantity into its own component

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,5 +1,6 @@
-import { FRUIT_IMAGES, FruitType } from "../models/Fruit"
+import { FruitType } from "../models/Fruit"
 import { useStore } from "../store"
+import { FruitQuantity } from "./FruitQuantity"
 
 const MAX_INVENTORY_SIZE = 8
 
@@ -11,12 +12,7 @@ export function Inventory() {
   return (
     <div className="grid grid-cols-4 gap-2 w-full">
       {Object.entries(inventory).map(([fruitType, quantity]) => (
-        <div className="relative border border-zinc-800 rounded-lg">
-          <img src={FRUIT_IMAGES[fruitType as FruitType]} alt={fruitType} key={fruitType} className="w-full h-full" />
-          <div className="absolute -top-1 -right-1 text-white text-sm font-bold bg-zinc-700 rounded-sm px-0.5 leading-snug">
-            {quantity}
-          </div>
-        </div>
+        <FruitQuantity fruitType={fruitType as FruitType} quantity={quantity} />
       ))}
       {emptySlots.map((_, index) => (
         <div key={index} className="w-full border border-zinc-800 rounded-lg aspect-square" />
