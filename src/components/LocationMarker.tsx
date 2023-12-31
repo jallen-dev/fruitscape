21da9cbc9e95a18d3cc70f1141d92d991f563739
Justin Allen <jallen@jallen.dev>
2023-12-31 03:06:56 -0500

    Fix location indicator

--- src/components/LocationMarker.tsx
+++ src/components/LocationMarker.tsx
@@ -22,7 +22,7 @@ export function LocationMarker({ location }: { location?: { x: number; y: number
 
   return (
     <Sprite
-      image="set2/tile_0060.png"
+      image="1_60.png"
       x={player.destination.x * 32}
       y={player.destination.y * 32}
       anchor={{ x: 0, y: 0 }}
