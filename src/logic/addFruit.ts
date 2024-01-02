a42a0f1508e7c4fbb8147541a82e322d10b6caeb
Justin Allen <jallen@jallen.dev>
2024-01-01 23:12:38 -0500

    Break up addFruit a bit

--- src/logic/addFruit.ts
+++ src/logic/addFruit.ts
@@ -3,19 +3,29 @@ import { ContextWithGameState, ActionContext } from "rune-games-sdk"
 import { GameState } from "./types"
 import { generateFruit, generateRecipe } from "../utils"
 
-export function addFruit(
-  { fruit }: { fruit: FruitType },
-  { game, playerId }: ContextWithGameState<ActionContext, GameState>
-) {
+type Params = {
+  fruit: FruitType
+}
+
+export function addFruit({ fruit }: Params, { game, playerId }: ContextWithGameState<ActionContext, GameState>) {
   if (!game.players[playerId].inventory[fruit]) {
     return
   }
 
+  // remove fruit from player inventory
   game.players[playerId].inventory[fruit]! -= 1
   if (game.players[playerId].inventory[fruit] === 0) {
     delete game.players[playerId].inventory[fruit]
   }
 
+  addPlayersFruitToIngredients(game, playerId, fruit)
+
+  if (isAllIngredientsAdded(game)) {
+    generateNewRecipe(game)
+  }
+}
+
+function addPlayersFruitToIngredients(game: GameState, playerId: string, fruit: FruitType) {
   game.contributedIngredients[fruit] = (game.contributedIngredients[fruit] ?? 0) + 1
   game.events.push({
     id: game.eventId,
@@ -26,18 +36,21 @@ export function addFruit(
     time: Rune.gameTime(),
   })
   game.eventId += 1
+}
 
-  // check if all the ingredients have been added
-  const recipe = game.currentRecipe
+function isAllIngredientsAdded(game: GameState) {
   const contributedIngredients = game.contributedIngredients
-  for (const [fruit, amount] of Object.entries(recipe)) {
+  for (const [fruit, amount] of Object.entries(game.currentRecipe)) {
     if (amount > (contributedIngredients[fruit as FruitType] ?? 0)) {
-      return
+      return false
     }
   }
 
-  // generate a new recipe
-  const numIngredients = Object.keys(recipe).length + 1
+  return true
+}
+
+function generateNewRecipe(game: GameState) {
+  const numIngredients = Object.keys(game.currentRecipe).length + 1
   const newFruits = generateFruit(numIngredients)
   const newRecipe = generateRecipe(newFruits)
   game.currentRecipe = newRecipe
