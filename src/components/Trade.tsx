3da512ccec36e809584797e6380d77134d18a6b8
Justin Allen <jallen@jallen.dev>
2024-01-08 14:53:26 -0500

    Update todo: This bug got fixed when I refactored usePlayerMovement() 🙌

--- src/components/Trade.tsx
+++ src/components/Trade.tsx
@@ -4,8 +4,6 @@ import { Dialog } from "./Dialog"
 import { Inventory } from "./Inventory"
 import { SwapFruit } from "./SwapFruit"
 
-// TODO: there's a bug where if the player is standing on an NPC with the trade dialog closed,
-// and their inventory is updated (because there's a new recipe and they got a new fruit), the trade dialog will open.
 export function Trade() {
   const playerId = useStore((state) => state.yourPlayerId)
   const inventory = useStore((state) => state.game.players[playerId]?.inventory)
